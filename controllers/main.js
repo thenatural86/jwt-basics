40a7097c6fd4e64da7d72be4cdc78eb19c361bde
thenatural86 <philip.smalls86@gmail.com>
2024-03-18 17:51:45 -0300

    auth headers

--- controllers/main.js
+++ controllers/main.js
@@ -19,6 +19,15 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
+  const authHeaders = req.headers.auth
+
+  if (!authHeaders || !authHeaders.startsWith('Bearer ')) {
+    throw new CustomApiError('No token provided', 401)
+  }
+
+  const token = authHeaders.split(' ')[1]
+  console.log(token)
+
   const luckyNumber = Math.floor(Math.random() * 100)
   res.status(200).json({
     msg: 'Hello, John Doe',
