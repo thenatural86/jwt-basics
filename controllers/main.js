9bc5e0dcbdc57a79a2cc0afc4ea315254f16c3b5
thenatural86 <philip.smalls86@gmail.com>
2024-03-19 10:10:47 -0300

    verify token

--- controllers/main.js
+++ controllers/main.js
@@ -19,20 +19,29 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-  const authHeaders = req.headers.auth
+  const authHeaders = req.headers.authorization
 
+  if (!authHeaders) {
+    console.log('no headers')
+  }
   if (!authHeaders || !authHeaders.startsWith('Bearer ')) {
     throw new CustomApiError('No token provided', 401)
   }
 
   const token = authHeaders.split(' ')[1]
-  console.log(token)
 
-  const luckyNumber = Math.floor(Math.random() * 100)
-  res.status(200).json({
-    msg: 'Hello, John Doe',
-    secret: `Here is you authorized data, your lucky number is ${luckyNumber}`,
-  })
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const luckyNumber = Math.floor(Math.random() * 100)
+
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is you authorized data, your lucky number is ${luckyNumber}`,
+    })
+  } catch (error) {
+    throw new CustomApiError('No authorized to access this route', 401)
+  }
 }
 
 module.exports = { login, dashboard }
