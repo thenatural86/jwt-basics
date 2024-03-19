cb184a832d306aa8d08b49ffa04c31fc5a0e9fa3
thenatural86 <philip.smalls86@gmail.com>
2024-03-19 11:59:44 -0300

    more error codes

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad-request')
+const UnAuthenticatedError = require('./unauthenticated')
+
+module.exports = { CustomAPIError, BadRequest, UnAuthenticatedError }
